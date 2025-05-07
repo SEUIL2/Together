b4ba23c1fae9810a4b6c01e30ef4018c7f8a4dd1
mooyoung01 <mooman01@naver.com>
2025-05-07 21:18:38 +0900

    CorsConfig 수정

--- backEnd/together/src/main/java/com/together/systemConfig/CorsConfig.java
+++ backEnd/together/src/main/java/com/together/systemConfig/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.together.systemConfig;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,13 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    public WebMvcConfigurer corsConfigurer() { //무영 수정 : PATCH , OPTIONS 추가
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 API 허용
                         .allowedOrigins("http://localhost:5173") // Vue 서버 주소
-                        .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // PATCH, OPTIONS 추가
+                        .allowedHeaders("*") // 요청 헤더 모두 허용
                         .allowCredentials(true);
             }
         };

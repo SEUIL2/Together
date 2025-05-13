77121cc54d8b9e66a11176edcc38b0ce1cfc31fd
SEUIL <sone91961@gmail.com>
2025-05-13 18:39:06 +0900

    스일작업 1. auth/me 에서 프로젝트 아이디도 반환되게 수정 1.1 학생은 단일 프로젝트로 반환 1.2 교수는 여러개의 프로젝트가 반환

--- backEnd/together/src/main/java/com/together/user/professor/ProfessorRepository.java
+++ backEnd/together/src/main/java/com/together/user/professor/ProfessorRepository.java
@@ -1,7 +1,11 @@
 package com.together.user.professor;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ProfessorRepository extends JpaRepository<ProfessorEntity, Long> {

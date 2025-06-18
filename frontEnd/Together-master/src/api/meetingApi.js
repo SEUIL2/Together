10d151c974fa45cef47038d07e46f8119d869819
mooyoung01 <mooman01@naver.com>
2025-06-18 20:40:11 +0900

    주소변경

--- frontEnd/Together-master/src/api/meetingApi.js
+++ frontEnd/Together-master/src/api/meetingApi.js
@@ -4,7 +4,8 @@ import axios from 'axios'
 const token = localStorage.getItem('authHeader') // 여기만 바꾸면 돼!
 
 const api = axios.create({
-  baseURL: 'http://localhost:8081/meeting',
+  // baseURL: 'http://localhost:8081/meeting', // 백엔드 주소
+  baseURL: 'http://25.12.59.4:3000/meeting', // 건우 주소
   withCredentials: true,
   headers: {
     Authorization: token ? token : ''

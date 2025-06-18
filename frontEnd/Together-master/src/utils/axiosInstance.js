42e5ce841927fa079a477482ba059f34c1a262f2
juquett <juquett@gmail.com>
2025-06-18 17:16:21 +0900

    d

--- frontEnd/Together-master/src/utils/axiosInstance.js
+++ frontEnd/Together-master/src/utils/axiosInstance.js
@@ -8,5 +8,15 @@ const instance = axios.create({
         'Content-Type': 'application/json',
     },
 })
+// 모든 요청에 로컬 스토리지의 인증 헤더를 자동 포함한다
+instance.interceptors.request.use(config => {
+    const token = localStorage.getItem('authHeader')
+    if (token) {
+        config.headers.Authorization = token
+    } else {
+        delete config.headers.Authorization
+    }
+    return config
+})
 
 export default instance
